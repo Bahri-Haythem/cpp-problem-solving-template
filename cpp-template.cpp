079848fe8a14831e72fe99600a2167a912684abf
Haythem Bahri <haythem.bahri8@hotmail.com>
2020-11-25 19:26:35 +0100

    first commit

---
+++ cpp-template.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+//#include "ext/pb_ds/tree_policy.hpp"
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef map<int,int> mii;
+
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+long long INFF = 1000000000000000005LL;
+const double pi=acos(-1.0);
+
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i) // 0 based indexing
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i) // 1 based indexing
+#define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i) // closed interver from l to r r inclusive
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) // reverse 0 based.
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i) // reverse 1 based
+#define F first
+#define S second
+#define mp make_pair
+#define pb push_back
+#define pqb priority_queue<int>
+#define pqs priority_queue<int,vi,greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define all(v) v.begin(), v.end()
+#define sz(v) (int)v.size()
+#define SORT(v) sort(ALL(v))
+#define SORTC(v, c) sort(ALL(v), c)
+#define REVERSE(v) reverse(ALL(v))
+#define test(x) int x; cin>>x; while(x--)
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+//change path
+//#define io_file freopen("D:/Coding Problems/Contest/input_file.in", "r", stdin);freopen("D:/Coding Problems/Contest/output_file.out", "w", stdout)
+
+int main()
+{
+    //cout.precision(10);cout << fixed;
+    fast
+    //main code
+}
